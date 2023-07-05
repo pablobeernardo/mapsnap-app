1f1568181d4385aeededf54a1b5a451039b2801f
pablobeernardo <pablobeernardo@gmail.com>
2023-07-04 21:48:30 -0300

    iniciando projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import * as Location from 'expo-location';
+import { Camera } from 'expo-camera';
 
-export default function App() {
+const App = () => {
+  const [markers, setMarkers] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [isCameraVisible, setCameraVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  const getLocationPermission = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permissão de localização não concedida');
+    } else {
+      getCurrentLocation();
+    }
+  };
+
+  const getCurrentLocation = async () => {
+    const { coords } = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = coords;
+    setCurrentLocation({ latitude, longitude });
+  };
+
+  const handleAddMarker = () => {
+    if (currentLocation && capturedImage) {
+      const newMarker = {
+        id: markers.length.toString(),
+        coordinate: currentLocation,
+        imageUri: capturedImage,
+      };
+      setMarkers([...markers, newMarker]);
+    }
+    setCameraVisible(false);
+  };
+
+  const handleOpenCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permissão da câmera não concedida');
+    } else {
+      setCameraVisible(true);
+    }
+  };
+
+  const handleCaptureImage = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setCapturedImage(photo.uri);
+      handleAddMarker();
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {currentLocation ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {markers.map(marker => (
+            <Marker
+              key={marker.id}
+              coordinate={marker.coordinate}
+              image={marker.imageUri}
+            />
+          ))}
+        </MapView>
+      ) : (
+        <Text style={styles.loadingText}>Carregando mapa...</Text>
+      )}
+
+      {!isCameraVisible && (
+        <TouchableOpacity style={styles.buttonContainer} onPress={handleOpenCamera}>
+          <View style={styles.iconContainer}>
+            <Icon name="camera" size={30} color="#FFFFFF" />
+          </View>
+        </TouchableOpacity>
+      )}
+
+      {isCameraVisible && (
+        <Camera
+          ref={cameraRef}
+          style={styles.camera}
+          type={Camera.Constants.Type['back']}
+          onCameraReady={() => console.log('Câmera pronta')}
+          onMountError={error => console.log('Erro ao montar a câmera:', error)}
+        >
+          <TouchableOpacity style={styles.captureButton} onPress={handleCaptureImage}>
+            <Text style={styles.captureButtonText}>Capturar</Text>
+          </TouchableOpacity>
+        </Camera>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  map: {
+    flex: 1,
+  },
+  loadingText: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 16,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 40,
+    right: 30,
+    backgroundColor: '#303F9F',
+    borderRadius: 40,
+    padding: 15,
+    elevation: 5,
+  },
+  iconContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  captureButton: {
+    position: 'absolute',
+    bottom: 30,
+    alignSelf: 'center',
+    backgroundColor: '#303F9F',
+    borderRadius: 40,
+    padding: 15,
+    elevation: 5,
+  },
+  captureButtonText: {
+    color: '#FFFFFF',
   },
 });
+
+export default App;
